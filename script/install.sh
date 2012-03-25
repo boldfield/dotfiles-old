3bc8cd9a98734fd3446e913fdb9b1460c38d8e3b
Collin Watson <watson.collin@gmail.com>
2012-03-25 16:53:31 -0700

    Code clean up

--- script/install.sh
+++ script/install.sh
@@ -1,85 +1,46 @@
 #!/usr/bin/env sh
 
-ret=0
-
 [ -f "$HOME/.dotrc" ] && source $HOME/.dotrc
 
+
 # 1. Verify dotfiles directory doesn't exist
+
 dot="$DOTFILES"
 [ ! "$dot" ] && dot="$HOME/.dotfiles"
-
-if [ -d "$dot" ]; then
-    echo "Error: dotfile directory exists."
-    exit 1
-fi
+[ -d "$dot" ] && echo "Error: dotfile directory exists." && exit 1
 
 
-# 2. Verify mkdir command
+# 2. Location required commands
+
 mkdir="${MKDIR}"
+[ -z "$mkdir" ] && mkdir=`which mkdir 2> /dev/null`
+[ -z "$mkdir" ] && echo "Error: Missing mkdir command" && exit 1
 
-if [ -z "$mkdir" ]; then
-  mkdir=`which mkdir 2>&1`
-  ret=$?
-fi
-
-if [ $? -gt 0 ]; then
-    echo "Error: Missing mkdir command"
-    exit 1
-fi
-
-
-# 3. Verify curl command
-curl="${CURL}"
-
-if [ -z "$curl" ]; then
-  curl=`which curl 2>&1`
-  ret=$?
-fi
-
-if [ $? -gt 0 ]; then
-    echo "Error: Missing curl command"
-    exit 1
-fi
-
-
-# 4. Verify curl command
-curl="${CURL}"
-
-if [ -z "$curl" ]; then
-  curl=`which curl 2>&1`
-  ret=$?
-fi
-
-if [ $? -gt 0 ]; then
-    echo "Error: Missing curl command"
-    exit 1
-fi
-
-
-# 5. Verify make command
 make="${MAKE}"
+[ -z "$make" ] && make=`which make 2> /dev/null`
+[ -z "$make" ] && echo "Error: Missing make command" && exit 1
 
-if [ -z "$make" ]; then
-  make=`which make 2>&1`
-  ret=$?
-fi
+curl="${CURL}"
+[ -z "$curl" ] && curl=`which curl 2> /dev/null`
+[ -z "$curl" ] && echo "Error: Missing curl command" && exit 1
 
-if [ $? -gt 0 ]; then
-    echo "Error: Missing make command"
-    exit 1
-fi
+git="${GIT}"
+[ -z "$git" ] && git=`which git 2> /dev/null`
 
 
-# 6. Download dotfiles
+# 3. Get
 
 echo "Downloading..."
 
-if [ `which git 2>&1` ]; then
-    git clone https://github.com/collinwat/dotfiles.git $dot 1> /dev/null 2> /dev/null
-else
+if [ -z "$git" ]; then
     $mkdir -p "$dot" && \
         $curl -Ls https://github.com/collinwat/dotfiles/tarball/master | \
         $tar -C "$dot" --strip=1 -xzf -
+else
+    git clone https://github.com/collinwat/dotfiles.git $dot 1> /dev/null 2> /dev/null
 fi
+
+
+# 4. Install
 
 cd "$dot" && $make install
